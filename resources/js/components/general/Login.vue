5c44f67157ce1c0c69fafc6bd46fd787ea038e93
snretuerma <snretuerma@up.edu.ph>
2021-03-04 15:51:40 +0800

    Add alert and validation errors to login form

--- resources/js/components/general/Login.vue
+++ resources/js/components/general/Login.vue
@@ -4,6 +4,14 @@
             Login
         </v-card-title>
         <v-card-text>
+            <alert-component
+                :show="login_failed"
+                :color="'red'"
+                :icon="'mdi-alert'"
+                :message="message"
+                :message_bag="error_bag"
+                @dismiss_alert="dismissAlert"
+            />
             <v-form @submit.prevent="login">
                 <v-container>
                     <v-row
@@ -14,9 +22,12 @@
                             cols="6"
                         >
                         <v-text-field
-                            v-model="form.username"
+                            v-model="username"
                             label="Username"
                             required
+                            :error-messages="usernameErrors"
+                            @input="$v.username.$touch()"
+                            @blur="$v.username.$touch()"
                         ></v-text-field>
                         </v-col>
                     </v-row>
@@ -28,9 +39,12 @@
                             cols="6"
                         >
                         <v-text-field
-                            v-model="form.password"
+                            v-model="password"
                             label="Password"
                             required
+                            :error-messages="passwordError"
+                            @input="$v.password.$touch()"
+                            @blur="$v.password.$touch()"
                         ></v-text-field>
                         </v-col>
                     </v-row>
@@ -53,26 +67,64 @@
 </template>
 
 <script>
+import AlertComponent from '../helpers/AlertComponent.vue'
+const { validationMixin, default: Vuelidate } = require('vuelidate')
+const { required, minLength } = require('vuelidate/lib/validators')
 export default {
+    components: { AlertComponent },
+    mixins: [validationMixin],
     data() {
         return {
-            form: {
-                username: null,
-                password: null,
-            }
+            username: null,
+            password: null,
+            login_failed: false,
+            error_bag: null,
+            message: null,
         }
     },
+    computed: {
+        usernameErrors () {
+            const errors = []
+            if (!this.$v.username.$dirty) return errors
+            !this.$v.username.required && errors.push('Username is required.')
+            return errors
+        },
+        passwordError () {
+            const errors = []
+            if (!this.$v.username.$dirty) return errors
+            !this.$v.password.minLength && errors.push('Password must be at least 6 characters long')
+            !this.$v.password.required && errors.push('Password is required.')
+            return errors
+        },
+    },
+    validations: {
+        username: {
+            required,
+        },
+        password: {
+            required,
+            minLength: minLength(6)
+        },
+    },
     methods: {
+        dismissAlert() {
+            this.login_failed = false;
+        },
         login() {
+            this.login_failed = false, this.error_bag = this.message =null;
+            this.$v.$touch()
             axios.get('/sanctum/csrf-cookie').then(response => {
-                axios.post('login', this.form)
+                axios.post('login', { username: this.username, password: this.password })
                 .then(response => {
                     if(this.$route.name != 'Dashboard') {
+                        this.username = this.password = this.error_bag  = this.message = null;
                         this.$router.push({name: 'Dashboard'});
                     }
                 })
                 .catch(error => {
-                    console.log("Front End Unauthenticated");
+                    this.login_failed = true;
+                    this.message = 'Authentication Failed!'
+                    this.error_bag = Object.values(error.response.data.errors).flat();
                 });
             });
         }
